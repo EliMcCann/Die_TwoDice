df7538e42f412f916e7a442d34c78ff7ceee9c9b
EliMcCann <143523063+EliMcCann@users.noreply.github.com>
2023-12-11 11:08:47 -0500

    Add files via upload

---
+++ TwoDice.java
@@ -0,0 +1,30 @@
+public class TwoDice 
+{
+    public static void main(String[] args)
+    {
+    // Variables and constants
+        Die firstDie = new Die();
+        Die secondDie = new Die();
+        String outcome;
+    // Processing phase
+        if(firstDie.getDieValue() > secondDie.getDieValue())
+        {
+            outcome = "The player wins the game!";
+        } // End of initial IF statement
+        else
+        {
+            if(firstDie.getDieValue() < secondDie.getDieValue())
+            {
+                outcome = "Sorry, the computer has won...";
+            } // End of nested IF
+            else
+            {
+                outcome = "We have a tie game!";
+            } // End of nested ELSE
+        } // End of initial ELSE statement
+    // Output phase
+        System.out.println("Player die roll is: " + firstDie.getDieValue());
+        System.out.println("Computer die roll is: " + secondDie.getDieValue());
+        System.out.println(outcome);
+    }    
+}
